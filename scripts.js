09f02196d214b1ee4038ebd5bc3791021994f999
Lucas Dasso <lucas.dasso@wunderman.com>
2018-08-14 19:48:12 -0300

    Rewrite the code to a editor preview layout

--- scripts.js
+++ scripts.js
@@ -1,74 +1,71 @@
   var vm = new Vue({
       el: '#wrapper',
       data: {
-          editor_html: '',
-          editor_data: '',
-          tab: {
-              title: '',
-              author: '',
-              chords: {
-                  diagrams: [],
-                  photos: []
-              },
-              verses: [],
-              verses_text: ''
-          }
+          source_tab: '',
+          title: 'Song title',
+          album: 'Album title',
+          author: 'Author name',
+          chords: []
       },
-      methods: {
-          parse_rawHTML: function () {
-              var result = parse_rawData(this.editor_html);
-              this.tab = result;
-              this.tab.verses_text =  result.verses;
-          },
-          parse_data: function () {
-            this.tab.verses = this.tab.verses_text.replace('<br/>', '\n').split('\n\n');
-          }
-      },
-      watch: {
-        tab: function(){
-            document.title = this.tab.title + (this.tab.author ? '_'+this.tab.author : '');
+      computed: {
+        print_title: function () {
+            var titles_items = [this.tab.author, this.tab.album, this.tab.title].filter(Boolean);
+            document.title = titles_items.length ? titles_items.join(' - ') : 'Tab2Print';
+        },
+        verses: function () {
+            this.chords = get_chords(this.source_tab);
+            return clean_input(this.source_tab).split('\n\n');
         }
-
       }
   });
 
 
-  function parse_rawData(html) {
-      var data = {
-          title: '',
-          author: '',
-          chords: {
-              diagrams: [],
-              photos: []
-          },
-          verses: []
-      };
+    function get_chords(str){
+        
+    }
 
-      html = $("<div/>").html(html);
-
-      [data.title, data.author] = $(html).find('#page_title span').map((key, value) => $(value).text()).get();
-      console.log(data);
-
-      data.chords.diagrams = $(html).find('#sticky_crd img').map((key, value) => 'https://www.ukulele-tabs.com/' + $(value).attr('src').replace('../', '')).get();
-
-      console.log(data.chords.diagrams);
-      data.chords.photos = $(html).find('#sticky_crd a').map((key, value) => 'https://www.ukulele-tabs.com' + $(value).attr('href')).get();
-
-
-      var raw_textlines = $('<div/>').html($(html).find('pre').html().replace(/<br>/mg, "\n")).text().trim();
-      raw_textlines = raw_textlines.split(/\r\n|\r|\n/g);
-
-      data.verses = raw_textlines
-          .join('<br/>')
-          .replace(/\\\[([^\]]+)\]/gm, '</span><span class="chord"><span>$1</span></span><span>')
-          .replace(/(<br\/?>){2,}/gm, '\n\n')
-          .split(/\n\n/);
-
-      data.verses = data.verses.map(function (val) {
-          var re = /<span><\/span>|<span>$|^<\/span>/gm;
-          return val.replace(re, '');
-      });
-
-
-      return data;
+  function clean_input(str) {
+      str = str.replace(/''/g, '"');
+      str = str.replace('<br/>', '\n')
+      return str;
   }
+
+  //   function parse_rawData(html) {
+  //       var data = {
+  //           title: '',
+  //           author: '',
+  //           chords: {
+  //               diagrams: [],
+  //               photos: []
+  //           },
+  //           verses: []
+  //       };
+
+  //       html = $("<div/>").html(html);
+
+  //       [data.title, data.author] = $(html).find('#page_title span').map((key, value) => $(value).text()).get();
+  //       console.log(data);
+
+  //       data.chords.diagrams = $(html).find('#sticky_crd img').map((key, value) => 'https://www.ukulele-tabs.com/' + $(value).attr('src').replace('../', '')).get();
+
+  //       console.log(data.chords.diagrams);
+  //       data.chords.photos = $(html).find('#sticky_crd a').map((key, value) => 'https://www.ukulele-tabs.com' + $(value).attr('href')).get();
+
+
+  //       var raw_textlines = $('<div/>').html($(html).find('pre').html().replace(/<br>/mg, "\n")).text().trim();
+  //       raw_textlines = raw_textlines.split(/\r\n|\r|\n/g);
+
+  //       data.verses = raw_textlines
+  //           .join('<br/>')
+  //           .replace(/\\\[([^\]]+)\]/gm, '</span><span class="chord"><span>$1</span></span><span>')
+  //           .replace(/(<br\/?>){2,}/gm, '\n\n')
+  //           .split(/\n\n/);
+
+  //       data.verses = data.verses.map(function (val) {
+  //           var re = /<span><\/span>|<span>$|^<\/span>/gm;
+  //           return val.replace(re, '');
+  //       });
+
+
+  //       return data;
+  //   }
