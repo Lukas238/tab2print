172708cdec19931788ace567c232980af401195d
Lucas Dasso <lucas.dasso@wunderman.com>
2018-08-15 18:25:06 -0300

    Updated

--- scripts.js
+++ scripts.js
@@ -5,24 +5,50 @@
           title: 'Song title',
           album: 'Album title',
           author: 'Author name',
-          chords: []
+          chords_visible: true
       },
       computed: {
-        print_title: function () {
-            var titles_items = [this.tab.author, this.tab.album, this.tab.title].filter(Boolean);
-            document.title = titles_items.length ? titles_items.join(' - ') : 'Tab2Print';
-        },
-        verses: function () {
-            this.chords = get_chords(this.source_tab);
-            return clean_input(this.source_tab).split('\n\n');
-        }
+          print_title: function () {
+              var titles_items = [this.tab.author, this.tab.album, this.tab.title].filter(Boolean);
+              document.title = titles_items.length ? titles_items.join(' - ') : 'Tab2Print';
+          },
+          verses: function () {
+              return clean_input(this.source_tab).split('\n\n');
+          },
+          chords: function(){
+            return get_chords(this.source_tab);
+          }
       }
   });
 
 
-    function get_chords(str){
-        
-    }
+  function get_chords(str) {
+    var chords = [];
+    str.split(/\n/).forEach(function (line, key) {
+        chords = [...chords, ...extract_tabs(line)]; 
+    });
+    chords = [... new Set(chords)];
+    chords = chords.filter(Boolean);
+    return chords;
+  }
+
+  /*
+    Source: https://github.com/oliverpool/guitar-tabs_songtex.js/blob/master/guitar-tabs_songtex.js
+ */
+  function extract_tabs(line) {
+      var reg = /^ *[A-Ga-g](#|b|&)?m?(sus|add|maj|aug|dim)?[0-9]?( *(-|\/) *[A-G](#|b)?)?( +[A-Ga-g](#|b|&)?m?(sus|add|maj|aug|dim)?[0-9]?( *(-|\/) *[A-G](#|b|&)?)? *)* *$/,
+          reguniq = /[A-Ga-g](#|b|&)?m?(sus|add|maj|aug|dim)?[0-9]?( *(-|\/) *[A-G](#|b|&)?)? *$/,
+          i,
+          tab;
+
+      if (line.match(reg)) {
+          i = line.search(reguniq);
+          tab = extract_tabs(line.substr(0, i - 1));
+          tab[i] = line.substr(i).trim().replace('b', '&').toLowerCase();
+          return tab;
+      }
+      return [];
+  }
 
   function clean_input(str) {
       str = str.replace(/''/g, '"');
