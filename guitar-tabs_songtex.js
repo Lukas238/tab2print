ea226d76a58ecc7a017e4a5df767a66e399d5dc6
Lucas Dasso <lucas.dasso@wunderman.com>
2018-08-16 18:23:12 -0300

    Added inline chords on the copy to better styling on columns

---
+++ guitar-tabs_songtex.js
@@ -0,0 +1,99 @@
+
+var plainTab2tex = (function () {
+    'use strict';
+
+    var module = {};
+
+    // Used to replace tabulations with spaces
+    function repeatString(str, num) {
+        return new Array( num + 1 ).join(str);
+    }
+
+    // replaces the tabulations with spaces
+    function clean_tabu(line) {
+        var res = line.replace(/^\t/g, repeatString(" ", 10)),
+            i = res.search(/\t/);
+        while (i >= 0) {
+            res = res.substr(0, i) + repeatString(" ", 8 - ((i + 6) % 8)) + res.substr(i + 1);
+            i = res.search(/\t/);
+        }
+        return res;
+    }
+
+    // some cleaning of the output text
+    function capitaliseFirstLetter(string) {
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+    function remove_trailing_punctuation(string) {
+        return string.replace(/(,|;|\.)*$/g, "");
+    }
+    function clean_string(line) {
+        var res = line;
+        res = capitaliseFirstLetter(res);
+        res = remove_trailing_punctuation(res);
+        return res;
+    }
+
+
+    // insert the guitar tabs into the current line
+    function insert_tabs_in_line(cur_line, cur_tabs) {
+        var j = cur_tabs.length,// index amoung the cur_tabs
+            l = cur_line.length;
+        while (j > 0) {
+            j -= 1;
+            if (cur_tabs[j] !== undefined) {
+                l = Math.min(j, l);//if the tabs are after the end of the text line, we insert them at the end
+                cur_line = cur_line.substr(0, l) + "<span class='chord'><span>" + cur_tabs[j] + "</span></span>" + cur_line.substr(l);
+            }
+        }
+        return cur_line;
+    }
+
+    // extract the tabs of the line into an array where the index is the position of the tab in the line
+    function extract_tabs(line) {
+        var reg = /^ *[A-Ga-g](#|b|&)?m?(sus|add|maj)?[0-9]?( *(-|\/) *[A-G](#|b)?)?( +[A-Ga-g](#|b|&)?m?(sus|add|maj)?[0-9]?( *(-|\/) *[A-G](#|b|&)?)? *)* *$/,
+            reguniq = /[A-Ga-g](#|b|&)?m?(sus|add|maj)?[0-9]?( *(-|\/) *[A-G](#|b|&)?)? *$/,
+            i,
+            tab;
+
+        if (line.match(reg)) {
+            i = line.search(reguniq);
+            tab = extract_tabs(line.substr(0, i - 1));
+            tab[i] = line.substr(i).trim().replace('b', '&');
+            return tab;
+        }
+        return [];
+    }
+
+    // inserts the tabs into the raw_lines according to the "patacrep formatting"
+    // input and output are arrays
+    module.parse = function (raw_lines) {
+        var i = raw_lines.length - 1,    // index amoung the lines of the raw text (we start with the last line)
+
+            cur_line = raw_lines[i],    // line where we currently insert the tabs
+
+            parsed_lines = [],          // lines that are already parsed
+            cur_tabs;
+
+        while (i > 0) {
+            i -= 1;
+            // for every "next line", we try to extract some guitar tabs
+            cur_tabs = extract_tabs(clean_tabu(raw_lines[i]));
+
+            if (cur_tabs.length > 0) {
+                // if we were able to parse, we insert the guitar tabs into the current line
+                cur_line = insert_tabs_in_line(cur_line, cur_tabs);
+            } else {
+                // else we just move on
+                parsed_lines.unshift(cur_line);
+                cur_line = clean_string(raw_lines[i]);
+            }
+            if (i === 0) {
+                // for the last iteration
+                parsed_lines.unshift(cur_line);
+            }
+        }
+        return parsed_lines;
+    };
+    return module;
+}());
--- scripts.js
+++ scripts.js
@@ -13,7 +13,9 @@
               document.title = titles_items.length ? titles_items.join(' - ') : 'Tab2Print';
           },
           verses: function () {
-              return clean_input(this.source_tab).split('\n\n');
+            //   return clean_input(this.source_tab).split('\n\n');
+            var output = plainTab2tex.parse(this.source_tab.split(/\r\n|\r|\n/g)).join('</br>').split('</br></br>');
+              return console.log(output) || output;
           },
           chords: function(){
             return get_chords(this.source_tab);
@@ -56,42 +58,15 @@
       return str;
   }
 
-  //   function parse_rawData(html) {
-  //       var data = {
-  //           title: '',
-  //           author: '',
-  //           chords: {
-  //               diagrams: [],
-  //               photos: []
-  //           },
-  //           verses: []
-  //       };
-
-  //       html = $("<div/>").html(html);
-
-  //       [data.title, data.author] = $(html).find('#page_title span').map((key, value) => $(value).text()).get();
-  //       console.log(data);
-
-  //       data.chords.diagrams = $(html).find('#sticky_crd img').map((key, value) => 'https://www.ukulele-tabs.com/' + $(value).attr('src').replace('../', '')).get();
-
-  //       console.log(data.chords.diagrams);
-  //       data.chords.photos = $(html).find('#sticky_crd a').map((key, value) => 'https://www.ukulele-tabs.com' + $(value).attr('href')).get();
-
-
-  //       var raw_textlines = $('<div/>').html($(html).find('pre').html().replace(/<br>/mg, "\n")).text().trim();
-  //       raw_textlines = raw_textlines.split(/\r\n|\r|\n/g);
-
-  //       data.verses = raw_textlines
-  //           .join('<br/>')
-  //           .replace(/\\\[([^\]]+)\]/gm, '</span><span class="chord"><span>$1</span></span><span>')
-  //           .replace(/(<br\/?>){2,}/gm, '\n\n')
-  //           .split(/\n\n/);
-
-  //       data.verses = data.verses.map(function (val) {
-  //           var re = /<span><\/span>|<span>$|^<\/span>/gm;
-  //           return val.replace(re, '');
-  //       });
-
-
-  //       return data;
-  //   }
+  plainTab2tex.insert_tabs_in_line = function(cur_line, cur_tabs) {
+    var j = cur_tabs.length,// index amoung the cur_tabs
+        l = cur_line.length;
+    while (j > 0) {
+        j -= 1;
+        if (cur_tabs[j] !== undefined) {
+            l = Math.min(j, l);//if the tabs are after the end of the text line, we insert them at the end
+            cur_line = cur_line.substr(0, l) + "[" + cur_tabs[j] + "]" + cur_line.substr(l);
+        }
+    }
+    return cur_line;
+}
