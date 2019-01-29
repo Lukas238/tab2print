fbb078f84257f999f9ea74c89b3421a77800e982
Lucas Dasso <DassoLucas@c238.com.ar>
2019-01-29 19:29:46 -0300

    Download image if not exist local

--- src/inc/chord.php
+++ src/inc/chord.php
@@ -1,36 +1,32 @@
 <?php
 
-define('API', '86a9106ae65537651a8e456835b316ab');
+include_once(dirname(__FILE__) . '/config.inc');
 
-$r = isset($_GET['r']) ? $_GET['r'] : '';
-$typ = isset($_GET['typ']) ? $_GET['typ'] : '';
+
+$r = isset($_GET['r']) ? $_GET['r'] : ''; //Chord
+$typ = isset($_GET['typ']) ? $_GET['typ'] : ''; //Chord type
+$filefullname = CHORD_IMAGES_FOLDER . '/'. strtoupper($r). strtolower($typ) . '.png'; //Chord image file name and path.
 
 
 if(!$r ){
     die('Missing parameters');
 }
 
-//XML file to load
-$api_url = 'http://www.ukulele-chords.com/get?ak='. API .'&r='.$r.'&typ='.$typ;
 
-//converts the specified XML file into a SimpleXMLElement object
-// $xml = simplexml_load_file($api_url);
-// echo $xml;
+//Serve the local file, if existe.
+if( !file_exists($filefullname)){
+    //Retrive remote image, save and serve localy
 
-$res = file_get_contents($api_url);
+    //XML file to load
+    $api_url = 'http://www.ukulele-chords.com/get?ak='. CHORDS_API_KEY .'&r='.$r.'&typ='.$typ;
+    $res = file_get_contents($api_url);
+    $src = preg_match('/<chord_diag_mini>(.+)<\/chord_diag_mini>/', $res , $matches);
+    $remoteImage = $matches[1];
+    
+    file_put_contents($filefullname, file_get_contents($remoteImage));
+}
 
-$src = preg_match('/<chord_diag_mini>(.+)<\/chord_diag_mini>/', $res , $matches);
+header("Content-type: image/png");
+echo readfile($filefullname);
 
-
-//Navigate through the tree to get your info (chord diagram here)
-//chord[0] returns the "main chord". For alternative positions, use a foreach() loop.
-// $res = $xml->chord[0]->chord_diag_mini;
-
-//print the result
-// echo $matches[1];
-
-$remoteImage = $matches[1];
-$imginfo = getimagesize($remoteImage);
-header("Content-type: {$imginfo['mime']}");
-readfile($remoteImage);
 ?>
