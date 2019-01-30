81da66013120d0c9c33f52e6dd5b042ab70d2aba
Lucas Dasso <DassoLucas@c238.com.ar>
2019-01-30 16:46:08 -0300

    Added fallback image if API return false

--- src/inc/chord.php
+++ src/inc/chord.php
@@ -6,6 +6,7 @@ include_once(dirname(__FILE__) . '/config.inc');
 $r = isset($_GET['r']) ? $_GET['r'] : ''; //Chord
 $typ = isset($_GET['typ']) ? $_GET['typ'] : ''; //Chord type
 $filefullname = CHORD_IMAGES_FOLDER . '/'. strtoupper($r). strtolower($typ) . '.png'; //Chord image file name and path.
+$falback_fullname = CHORD_IMAGES_FOLDER . '/not_found.png';
 
 
 if(!$r ){
@@ -20,10 +21,15 @@ if( !file_exists($filefullname)){
     //XML file to load
     $api_url = 'http://www.ukulele-chords.com/get?ak='. CHORDS_API_KEY .'&r='.$r.'&typ='.$typ;
     $res = file_get_contents($api_url);
-    $src = preg_match('/<chord_diag_mini>(.+)<\/chord_diag_mini>/', $res , $matches);
-    $remoteImage = $matches[1];
-    
-    file_put_contents($filefullname, file_get_contents($remoteImage));
+
+    if( strpos($res, '<chord>false</chord>' ) ){
+        $filefullname = $falback_fullname;
+    }else{
+        $src = preg_match('/<chord_diag_mini>(.+)<\/chord_diag_mini>/', $res , $matches);
+        $remoteImage = $matches[1];
+        
+        file_put_contents($filefullname, file_get_contents($remoteImage));
+    }
 }
 
 header("Content-type: image/png");
